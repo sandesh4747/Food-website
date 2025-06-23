f0b961d6791802466d5d82e84b58e9c5a63a6956
sandesh4747 <sangitthapa328@gmail.com>
2025-06-23 21:35:34 +0545

    changes

--- frontend/src/store/mainApi.js
+++ frontend/src/store/mainApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseUrl = (import.meta.env.MODE = "development"
-  ? "http://localhost:4000"
+  ? "http://localhost:4000/api/v1/auth"
   : "/api/v1/auth");
 
 export const mainApi = createApi({
